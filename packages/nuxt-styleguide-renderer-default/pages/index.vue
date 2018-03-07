199e94c9600588c95a3afa93651856096946b3a3
Lasse Diercks <github@lassediercks.de>
2018-03-07 14:18:58 +0100

    feat(Index): use wrap and introduce home

--- packages/nuxt-styleguide-renderer-default/pages/index.vue
+++ packages/nuxt-styleguide-renderer-default/pages/index.vue
@@ -1,16 +1,15 @@
 <template>
-    <div class="sum-wrap">
+    <Wrap>
+      <div class="home">
+
       <Header>Nexible</Header>
       <Grid>
-        <Card :style='{"--card-color": "var(--sum-green)"}'>Sketch Library</Card>
-        <Card :style='{"--card-color": "var(--sum-red)"}'>Colors</Card>
-        <Card :style='{"--card-color": "var(--sum-purple)"}'>Gitlab Repo</Card>
         <Card v-bind:href="$styleguide.path + '/components'" :style='{"--card-color": "var(--sum-brown)"}'>Components</Card>
       </Grid>
       <Footer></Footer>
-      <Font></Font>
-
     </div>
+
+    </Wrap>
 </template>
 
 <script>
@@ -18,49 +17,29 @@ import StyleguideNav from '../nav';
 import Card from '../card/card';
 import Grid from '../grid/grid';
 import Header from '../header/header';
-import Font from '../fonts/fonts';
 import Footer from '../footer/footer';
 import Headline from '../headline/headline';
+import Wrap from '../wrap/wrap';
 export default {
-  components: { StyleguideNav, Card, Header, Headline, Grid, Footer, Font },
+  components: {
+    StyleguideNav,
+    Card,
+    Header,
+    Headline,
+    Grid,
+    Footer,
+    Wrap,
+  },
 };
 </script>
 
 <style lang="scss">
-body{
-  margin: 0;
-  padding: 0;
-}
-.sum-wrap {
-  --sum-primary-color: #000;
-  --sum-primary-gray: #f5f5f5;
-  --sum-secondary-gray: #666;
-  --sum-third-gray: #333;
 
-  --sum-blue: #0785b9;
-  --sum-yellow: #ebc500;
-  --sum-red: #d53140;
-  --sum-turquoise: #55b0b7;
-  --sum-green: #00555f;
-  --sum-purple: #9c488c;
-  --sum-white: #fff;
-  --sum-brown: #8f5b00;
-  --sum-pink: #f08697;
-
-  --sum-font: "GothamHTF-Book", arial, sans-serif;
-  --sum-font-black: "GothamHTF-Black", arial, sans-serif;
-  --sum-font-bold: "GothamHFT-Bold", arial, sans-serif;
-  --sum-font-light: "GothamHTF-Light", arial, sans-serif;
-
-  font-family: var(--sum-font);
-  max-width: 1024px;
-  margin: 0 auto;
-
+.home{
   display: grid;
   min-height: 100vh;
   grid-template-rows: auto auto auto;
   grid-template-columns: 1fr;
   align-content: space-between;
 }
-
 </style>
