909df12292d364e9cbd829c568a8358d6e21a3eb
Hannes Diercks <github@xiphe.net>
2018-03-07 14:46:56 +0100

    feat(nuxt-styleguide): apply markdown to all component info descriptions

--- packages/nuxt-styleguide/src/buildProxyComponents.js
+++ packages/nuxt-styleguide/src/buildProxyComponents.js
@@ -6,6 +6,7 @@ import _template from 'lodash.template';
 import { parse } from 'vue-docgen-api';
 import chalk from 'chalk';
 import Deferred from './Deferred';
+import marked from 'marked';
 
 const styleguideSrcDir = path.resolve(__dirname, '..', 'src');
 const tmpDir = path.resolve(__dirname, '..', '.tmp');
@@ -40,9 +41,72 @@ function getRelPath(srcDir, extend) {
   }
 }
 
+function tryInline(desc) {
+  if (desc.split('\n').length > 1) {
+    return marked(desc);
+  }
+
+  return marked.inlineLexer(desc, []);
+}
+
+function applyMarkdownToTags(data) {
+  const retVal = {
+    ...data,
+    description: tryInline(data.description),
+    tags: Object.keys(data.tags || {}).reduce((memo, key) => {
+      memo[key] = data.tags[key].map((tag) => {
+        return {
+          ...tag,
+          description: tryInline(tag.description),
+        };
+      });
+
+      return memo;
+    }, {}),
+  };
+
+  if (retVal.type && retVal.type.names) {
+    retVal.type.name = retVal.type.names.join('|');
+    delete retVal.type.names;
+  }
+
+  if (retVal.type && retVal.type.name) {
+    retVal.type.name = retVal.type.name.split('|').join(' | ');
+  }
+
+  delete retVal.comment;
+
+  return retVal;
+}
+
+function applyMarkdown(data) {
+  return {
+    ...applyMarkdownToTags(data),
+    ...['events', 'props', 'slots'].reduce((memo1, type) => {
+      memo1[type] = Object.keys(data[type] || {}).reduce((memo, key) => {
+        memo[key] = applyMarkdownToTags(data[type][key]);
+
+        return memo;
+      }, {});
+
+      return memo1;
+    }, {}),
+    methods: (data.methods || []).reduce((memo, methodDesc) => {
+      const withMd = applyMarkdownToTags(methodDesc);
+
+      delete withMd.modifiers;
+      delete withMd.name;
+
+      memo[methodDesc.name] = withMd;
+
+      return memo;
+    }, {}),
+  };
+}
+
 function getInfo(file, relPath, log) {
   try {
-    return parse(file);
+    return applyMarkdown(parse(file));
   } catch (e) {
     if (log) {
       if (e.message.indexOf('SyntaxError: unknown: Unexpected token') === 0) {
