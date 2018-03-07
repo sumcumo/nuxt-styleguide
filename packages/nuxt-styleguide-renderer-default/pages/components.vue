5994a2a4bf913d64eb216bbe94738e4e4c3129ce
Lasse Diercks <github@lassediercks.de>
2018-03-07 14:29:52 +0100

    feat(Components): create overview

--- packages/nuxt-styleguide-renderer-default/pages/components.vue
+++ packages/nuxt-styleguide-renderer-default/pages/components.vue
@@ -1,19 +1,24 @@
 <template>
-<div>
-  <nav>
-    <ul>
-      <li v-for="(route) in $styleguide.routes" v-if="route.category === 'Components'">
-        <a v-bind:href="route.path">{{route.name}}</a>
-      </li>
-    </ul>
-  </nav>
-</div>
+  <Wrap>
+    <Header>Nexible</Header>
+      <Headline level="2">Components</Headline>
+      <nav>
+        <ul>
+          <li v-for="(route) in $styleguide.routes" v-if="route.category === 'Components'">
+            <a v-bind:href="route.path">{{route.name}}</a>
+          </li>
+        </ul>
+      </nav>
+    </Wrap>
 </template>
 
 <script>
+import Wrap from '../wrap/wrap';
+import Header from '../header/header';
+import Headline from '../headline/headline';
 
 export default {
-  components: {  },
+  components: { Wrap, Header, Headline },
 };
 </script>
 
