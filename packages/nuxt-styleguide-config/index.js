844419f224244ee73586e38617dbdb4a79bfc60e
Hannes Diercks <github@xiphe.net>
2018-03-08 11:58:57 +0100

    feat(nuxt-styleguide-config): add package

---
+++ packages/nuxt-styleguide-config/index.js
@@ -0,0 +1,70 @@
+const parseArgs = require('minimist');
+const Utils = require('nuxt').Utils;
+const resolve = require('path').resolve;
+const existsSync = require('fs').existsSync;
+
+const argv = parseArgs(process.argv.slice(2), {
+  alias: {
+    c: 'config-file',
+  },
+  string: ['c'],
+  default: {
+    c: 'nuxt.config.js',
+  },
+});
+
+const rootDir = resolve(argv._[0] || '.');
+const nuxtConfigFile = resolve(rootDir, argv['config-file']);
+
+let options = {};
+
+if (existsSync(nuxtConfigFile)) {
+  options = require(nuxtConfigFile);
+} else if (argv['config-file'] !== 'nuxt.config.js') {
+  Utils.fatalError('Could not load config file: ' + argv['config-file']);
+}
+
+if (typeof options.rootDir !== 'string') {
+  options.rootDir = rootDir;
+}
+
+if (typeof options.srcDir !== 'string') {
+  options.srcDir = rootDir;
+}
+
+const pkg = require(resolve(options.rootDir, 'package.json'));
+
+const moduleOptions = Object.assign(
+  {},
+  options.styleguide || {},
+  (options.modules || [])
+    .filter((module) => {
+      return (
+        module === '@sum.cumo/nuxt-styleguide' ||
+        (Array.isArray(module) && module[0] === '@sum.cumo/nuxt-styleguide')
+      );
+    })
+    .reduce((memo, module) => {
+      if (Array.isArray(module)) {
+        return Object.assign({}, memo, module[1] || {});
+      }
+
+      return memo;
+    }, {})
+);
+
+module.exports = Object.assign(
+  options,
+  pkg,
+  {
+    extends: [],
+    path: '/styleguide',
+    renderer: '@sum.cumo/nuxt-styleguide-renderer-default',
+  },
+  moduleOptions,
+  {
+    extends: !Array.isArray(moduleOptions.extends)
+      ? [moduleOptions.extends]
+      : moduleOptions.extends,
+  }
+);
