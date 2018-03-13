0f456333ee49780b6d2b53a1f11ccfd60fd7d434
Hannes Diercks <github@xiphe.net>
2018-03-13 13:06:43 +0100

    feat(nuxt-styleguide-files): add package
    as the successor of nuxt-styleguide-components

---
+++ packages/nuxt-styleguide-files/index.js
@@ -0,0 +1,147 @@
+const config = require('@sum.cumo/nuxt-styleguide-config');
+const chokidar = require('chokidar');
+const path = require('path');
+const EventEmitter = require('events');
+const minimatch = require('minimatch');
+
+function getRelPath(srcDir, extend) {
+  try {
+    require.resolve(extend);
+    return extend;
+  } catch (e) {
+    if (e.message.indexOf('Cannot find module') !== 0) {
+      throw e;
+    }
+
+    const relPath = path.relative(srcDir, extend);
+
+    if (relPath.indexOf('..') !== 0) {
+      return `~/${relPath}`;
+    }
+
+    return path.resolve(extend);
+  }
+}
+
+function toArray(thing) {
+  return Array.isArray(thing) ? thing : [thing];
+}
+
+function matchesAnyPattern(patterns) {
+  return (file) => {
+    return patterns.reduce((memo, pattern) => {
+      return memo ? memo : minimatch(file, pattern);
+    }, false);
+  };
+}
+
+module.exports = function getComponents(sourceDirs, patterns) {
+  const components = new EventEmitter();
+  const matches = matchesAnyPattern(toArray(patterns));
+
+  const sources = toArray(sourceDirs).map((sourceDir) => {
+    return {
+      importPath: sourceDir,
+      relPath: path.join(getRelPath(config.srcDir, sourceDir)),
+    };
+  });
+
+  const watcher = chokidar.watch(
+    sources.map(({ importPath }) => {
+      return path.join(importPath, '**');
+    })
+  );
+
+  let ready = false;
+  const files = [];
+
+  watcher
+    .on('add', (file) => {
+      if (!matches(file)) {
+        return;
+      }
+
+      const source = sources.find(({ importPath }) => {
+        return file.indexOf(importPath) === 0;
+      });
+      const sourceIndex = sources.indexOf(source);
+
+      const name = path.basename(file, path.extname(file));
+
+      const fileObj = {
+        file,
+        sourceIndex,
+        name,
+        relPath: path.join(
+          source.relPath,
+          path.relative(source.importPath, file)
+        ),
+      };
+
+      components.emit('add', fileObj);
+      files.push(fileObj);
+
+      if (ready) {
+        update();
+      }
+    })
+    .on('unlink', (file) => {
+      if (!matches(file)) {
+        return;
+      }
+
+      const fileObj = files.find(({ file: f }) => f === file);
+
+      components.emit('unlink', fileObj);
+      files.splice(files.indexOf(fileObj), 1);
+
+      update();
+    })
+    .on('change', (file) => {
+      if (!matches(file)) {
+        return;
+      }
+
+      files.find(({ file: f }) => f === file).upToDate = false;
+
+      update();
+    })
+    .on('error', (error) => components.emit('error', error))
+    .on('ready', () => {
+      if (!config.dev) {
+        watcher.close();
+      }
+      update();
+      ready = true;
+      components.emit('ready');
+    });
+
+  function update() {
+    const existingNames = [];
+    const sourceFiles = files
+      .sort(({ sourceIndex: a }, { sourceIndex: b }) => {
+        return b - a;
+      })
+      .filter((entry) => {
+        if (existingNames.indexOf(entry.name) === -1) {
+          existingNames.push(entry.name);
+          return true;
+        }
+
+        return false;
+      });
+
+    sourceFiles
+      .filter((entry) => {
+        return !entry.upToDate;
+      })
+      .forEach((file) => {
+        components.emit('update', file);
+        file.upToDate = true;
+      });
+
+    components.emit('updateAll', sourceFiles);
+  }
+
+  return components;
+};
