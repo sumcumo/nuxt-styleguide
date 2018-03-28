0b3e242fb56ad07393b967b18d63f15c24538c3b
Lasse Diercks <github@lassediercks.de>
2018-03-28 13:49:34 +0200

    feat(nuxt-styleguide-renderer-default): introduce frame

---
+++ packages/nuxt-styleguide-renderer-default/frame.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="frame">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};</script>
+
+<style lang="css">
+.frame{
+  max-width: 120ch;
+  margin: 0 auto;
+  font-family: sans-serif;
+}
+</style>
