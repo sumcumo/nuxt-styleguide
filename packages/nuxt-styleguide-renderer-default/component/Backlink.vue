ee6a8765ec89a64c54f64020aaa5ae28f532b1e3
Lasse Diercks <github@lassediercks.de>
2018-03-28 17:13:19 +0200

    feat(nuxt-styleguide-renderer-default): introduce Backlink

---
+++ packages/nuxt-styleguide-renderer-default/component/Backlink.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+  <a class="backlink" :href="this.href"><span class="backlink-icon">&lt;</span>Back</a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+    },
+  },
+};</script>
+
+<style lang="css">
+  .backlink{
+    text-decoration: none;
+    padding: .2em 0;
+    font-size: 20px;
+    color: black;
+    font-weight: 900;
+    text-transform: uppercase;
+  }
+  .backlink-icon{
+    margin-right: .3em;
+  }
+</style>
