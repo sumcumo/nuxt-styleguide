976280afa998fa0ca57f54a129ffe6a28d319112
Hannes Diercks <github@xiphe.net>
2018-04-04 12:02:01 +0200

    feat(nuxt-styleguide-renderer-default): add Icon renderer

---
+++ packages/nuxt-styleguide-renderer-default/Icon.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>{{name}}</h1>
+  <img :src="iconUrl" />
+</div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: String,
+    iconUrl: String,
+    importPath: String,
+  },
+};</script>
+
+
+
+<style>
+</style>
