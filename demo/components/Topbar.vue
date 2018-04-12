1524053e43fc1845d255f6d8c5eb8c51902061ba
Lasse Diercks <github@lassediercks.de>
2018-04-12 11:38:54 +0200

    demo: introduce fixed topbar

---
+++ demo/components/Topbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="topbar">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Topbar'
+}
+</script>
+
+
+<style>
+    .topbar{
+        top: 0;
+        width: 100%;
+        left: 0;
+        position: fixed;
+        height: 70px;
+        background: #fff;
+        box-shadow: 0 0 5px 5px rgba(0,0,0,.3);
+    }
+</style>
