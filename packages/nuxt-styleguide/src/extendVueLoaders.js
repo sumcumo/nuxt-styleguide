152fe2f1994ce7e9f3183ce5094af1b6c43c8b45
Hannes Diercks <github@xiphe.net>
2018-05-09 16:16:09 +0200

    fix(nuxt-styleguide): make compatible to nuxt@2

--- packages/nuxt-styleguide/src/extendVueLoaders.js
+++ packages/nuxt-styleguide/src/extendVueLoaders.js
@@ -6,15 +6,31 @@ export default function addStyleguideLoaders(module) {
       (rule) => rule.loader === 'vue-loader'
     )
 
-    vueLoaderRule.options.loaders['styleguide-doc'] = path.resolve(
-      __dirname,
-      'loaders',
-      'doc-loader.js'
-    )
-    vueLoaderRule.options.loaders['styleguide-states'] = path.resolve(
-      __dirname,
-      'loaders',
-      'states-loader.js'
-    )
+    if (
+      vueLoaderRule &&
+      vueLoaderRule.options &&
+      vueLoaderRule.options.loaders
+    ) {
+      vueLoaderRule.options.loaders['styleguide-doc'] = path.resolve(
+        __dirname,
+        'loaders',
+        'doc-loader.js'
+      )
+      vueLoaderRule.options.loaders['styleguide-states'] = path.resolve(
+        __dirname,
+        'loaders',
+        'states-loader.js'
+      )
+    }
+
+    config.module.rules.push({
+      resourceQuery: /blockType=styleguide-doc/,
+      loader: path.resolve(__dirname, 'loaders', 'doc-loader.js'),
+    })
+
+    config.module.rules.push({
+      resourceQuery: /blockType=styleguide-states/,
+      loader: path.resolve(__dirname, 'loaders', 'states-loader.js'),
+    })
   })
 }
