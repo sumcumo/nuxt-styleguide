24dcd974518fdc9ab4872ab18811959f72422827
Hannes Diercks <github@xiphe.net>
2018-05-22 14:55:17 +0200

    fix(nuxt-custom-route-folder): use latest version of rxjs
    and also stop using extendRoutes callback
    (seems not to work as expected in nuxt@2)

--- packages/nuxt-custom-route-folder/src/getRoutes.js
+++ packages/nuxt-custom-route-folder/src/getRoutes.js
@@ -1,36 +1,45 @@
 import CHUNK_PREFIX from './chunkPrefix'
 
-export default function getRoutes(extendRoutes) {
+export default function getRoutes(nuxt) {
   if (!getRoutes.cache) {
     const customRoutes = []
 
+    let builder = null
     const update = () => {
-      extendRoutes((routes) => {
-        customRoutes.forEach((route) => {
-          if (!routes.includes(route)) {
-            const conflictingRoute = routes.find(
-              ({ path: p }) => p === route.path
-            )
-
-            if (!conflictingRoute) {
-              routes.push(route)
-            } else if ((conflictingRoute.priority || -1) <= route.priority) {
-              routes.splice(routes.indexOf(conflictingRoute), 1)
-              routes.push(route)
-            }
-          }
-        })
-
-        routes.forEach((route) => {
-          if (
-            route.chunkName.indexOf(CHUNK_PREFIX) === 0 &&
-            !customRoutes.includes(route)
-          ) {
-            routes.splice(routes.indexOf(route), 1)
-          }
-        })
-      })
+      if (builder) {
+        builder.generateRoutesAndFiles()
+      }
     }
+
+    nuxt.hook('build:done', (b) => {
+      builder = b
+    })
+
+    nuxt.hook('build:extendRoutes', (routes) => {
+      customRoutes.forEach((route) => {
+        if (!routes.includes(route)) {
+          const conflictingRoute = routes.find(
+            ({ path: p }) => p === route.path
+          )
+
+          if (!conflictingRoute) {
+            routes.push(route)
+          } else if ((conflictingRoute.priority || -1) <= route.priority) {
+            routes.splice(routes.indexOf(conflictingRoute), 1)
+            routes.push(route)
+          }
+        }
+      })
+
+      routes.forEach((route) => {
+        if (
+          route.chunkName.indexOf(CHUNK_PREFIX) === 0 &&
+          !customRoutes.includes(route)
+        ) {
+          routes.splice(routes.indexOf(route), 1)
+        }
+      })
+    })
 
     getRoutes.cache = {
       add(route) {
