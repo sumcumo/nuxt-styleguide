5cf70f64aa8a77c9b24d58d362e42f61bb758d99
Hannes Diercks <github@xiphe.net>
2018-05-24 14:36:00 +0200

    feat(nuxt-styleguide): treat component states as es module
    in order to suppoert imports, use of other modules and whatever comes to mind
    
    BREAKING CHANGE:
    styleguide-states blocks in vue files must now `export default` the array of states
    previously:
    ```
    <styleguide-states>
    [{ /* ... */ }]
    </styleguide-states>
    ```
    
    now:
    ```
    <styleguide-states>
    export default [{ /* ... */ }]
    </styleguide-states>
    ```

--- packages/nuxt-styleguide/src/loaders/states-loader.js
+++ packages/nuxt-styleguide/src/loaders/states-loader.js
@@ -1,18 +1,42 @@
+import { transform } from 'babel-core'
+
+async function load(source, inputSourceMap) {
+  const { code } = await transform(source, {
+    presets: [
+      [
+        'env',
+        {
+          targets: {
+            esmodules: false,
+          },
+        },
+      ],
+    ],
+    plugins: ['transform-object-rest-spread'],
+    inputSourceMap,
+  })
+
+  return `
+    const states = {};
+
+    (function(exports) {
+      ${code}
+    })(states)
+
+    module.exports = function(Component) {
+      if (!Component.options.__styleguide) {
+        Component.options.__styleguide = {};
+      }
+      Component.options.__styleguide.states = states.default;
+    }`
+}
+
 module.exports = function statesLoader(source, map) {
-  try {
-    this.callback(
-      null,
-      `
-      const states = ${source};
-      module.exports = function(Component) {
-        if (!Component.options.__styleguide) {
-          Component.options.__styleguide = {};
-        }
-        Component.options.__styleguide.states = states
-      }`,
-      map
-    )
-  } catch (e) {
-    this.callback(e)
-  }
+  const callback = this.async()
+
+  load(source, map)
+    .then((result) => {
+      return callback(null, result, map)
+    })
+    .catch(callback)
 }
